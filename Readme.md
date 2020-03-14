08fc0a0e2821487a915f89562d4cfdff74aea2c5
Love <quantumlove42@gmail.com>
2020-03-14 16:58:58 +0100

    Improved the workflow, can now import module

--- Readme.md
+++ Readme.md
@@ -2,6 +2,8 @@ Very "work in progress", but saves you a looot of hassle if you are on Windows /
 
 Not at all tested but we do it as we go!
 
+To "install" the module copt the repo to a folder in your module path (`$env:PSModulePath -split ';'`);
+
 Thank you [@paulbouwer](https://github.com/paulbouwer/k8s-cli-toolset/blob/master/Dockerfile) for the kubens/kubctx code
 
 Thank you [@sjawhar](https://github.com/sjawhar/dockerfiles) for the asess code!
--- dockershell.psm1
+++ dockershell.psm1
@@ -1,38 +1,42 @@
 # This is work in progress because need to export the functions properly
-function RunService {
-    param(
-         [string]
-         [Parameter(Mandatory=$true, Position=0)]
-         $serviceName,
-         [string[]]
-         [Parameter(Position=1, ValueFromRemainingArguments)]
-         $args
-    )
-    if ($args.count == 0) {
-        $args = @("")
-    }
-    docker-compose.exe -f .\docker-compose.yaml run --rm $serviceName $args[0]
-}
+# function RunService {
+#     param(
+#          [string]
+#          [Parameter(Mandatory=$true, Position=0)]
+#          $serviceName,
+#          [string[]]
+#          [Parameter(Position=1, ValueFromRemainingArguments)]
+#          $args
+#     )
+#     if ($args.count == 0) {
+#         $args = @("")
+#     }
+#     docker-compose.exe -f $PSScriptRoot\docker-compose.yaml run --rm $serviceName $args[0]
+# }
 
-function asess { RunService "asess" $args }
-Export-ModuleMember -Function 'asess'
-function helm { RunService "helm" $args }
-Export-ModuleMember -Function 'helm'
-function stern { RunService "stern" $args }
-Export-ModuleMember -Function 'stern'
-function kubectl { RunService "kubectl" $args }
-Export-ModuleMember -Function 'kubectl'
-function kubens { RunService "kubens" $args }
-Export-ModuleMember -Function 'kubens'
-function kubectx { RunService "kubectx" $args }
-Export-ModuleMember -Function 'kubectx'
+# function asess { RunService "asess" $args }
+# Export-ModuleMember -Function 'asess'
+# function helm { RunService "helm" $args }
+# Export-ModuleMember -Function 'helm'
+# function stern { RunService "stern" $args }
+# Export-ModuleMember -Function 'stern'
+# function kubectl { RunService "kubectl" $args }
+# Export-ModuleMember -Function 'kubectl'
+# function kubens { RunService "kubens" $args }
+# Export-ModuleMember -Function 'kubens'
+# function kubectx { RunService "kubectx" $args }
+# Export-ModuleMember -Function 'kubectx'
 
 # These work, copy to your powershell and run them, then execute the commands 
 # TODO: Implement dwd so you don't need to be in this directory for that
 
-function asess { docker-compose.exe -f .\docker-compose.yaml run --rm asess $args }
-function helm { docker-compose.exe -f .\docker-compose.yaml run --rm helm $args }
-function kubectl { docker-compose.exe -f .\docker-compose.yaml run --rm kubectl $args }
-function stern { docker-compose.exe -f .\docker-compose.yaml run --rm stern $args }
-function kubens { docker-compose.exe -f .\docker-compose.yaml run --rm kubens $args }
-function kubectx { docker-compose.exe -f .\docker-compose.yaml run --rm kubectx $args }
+function asess { 
+    $Env:CWD = $PWD.Path
+    docker-compose.exe -f $PSScriptRoot\docker-compose.yaml run --rm asess $args; 
+}
+Export-ModuleMember -Function 'asess'
+function helm { docker-compose.exe -f $PSScriptRoot\docker-compose.yaml run --rm helm $args }
+function kubectl { docker-compose.exe -f $PSScriptRoot\docker-compose.yaml run --rm kubectl $args }
+function stern { docker-compose.exe -f $PSScriptRoot\docker-compose.yaml run --rm stern $args }
+function kubens { docker-compose.exe -f $PSScriptRoot\docker-compose.yaml run --rm kubens $args }
+function kubectx { docker-compose.exe -f $PSScriptRoot\docker-compose.yaml run --rm kubectx $args }
